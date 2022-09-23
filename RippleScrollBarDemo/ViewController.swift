3cc17fcddc289d2dbcc5aafe7755643158956925
Somesh Karthik <somesh.karthik@cred.club>
2022-09-23 21:12:50 +0530

    Initial Commit

---
+++ RippleScrollBarDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RippleScrollBarDemo
+//
+//  Created by Somesh Karthik on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
