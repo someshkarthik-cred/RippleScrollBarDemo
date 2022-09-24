7112e2967988b48dd2bec9d0f869df12d734c2ed
Somesh Karthik <somesh.karthik@cred.club>
2022-09-24 07:49:05 +0530

    feat: Added RippleScrollBar

--- RippleScrollBarDemo/RippleScrollBar.swift
+++ RippleScrollBarDemo/RippleScrollBar.swift
@@ -5,4 +5,181 @@
 //  Created by Somesh Karthik on 23/09/22.
 //
 
-import Foundation
+import UIKit
+
+final class RippleScrollBar: UIView {
+    private let leftInstance = RippleReplicatorInstance()
+    private let rightInstance = RippleReplicatorInstance()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        addSubview(leftInstance)
+        addSubview(rightInstance)
+        
+        leftInstance.clipsToBounds = true
+        rightInstance.clipsToBounds = true
+        leftInstance.tintColor = UIColor(red: 114/255, green: 41/255, blue: 217/255, alpha: 1.0)
+        leftInstance.direction = .left
+        leftInstance.rippleLineWidth = 4
+        
+        rightInstance.direction = .right
+        rightInstance.rippleLineWidth = leftInstance.rippleLineWidth
+        rightInstance.tintColor = leftInstance.tintColor
+        
+        leftInstance.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leftInstance.leadingAnchor.constraint(equalTo: leadingAnchor),
+            leftInstance.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            leftInstance.bottomAnchor.constraint(equalTo: bottomAnchor),
+            leftInstance.topAnchor.constraint(equalTo: topAnchor)
+        ])
+        
+        rightInstance.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            rightInstance.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            rightInstance.trailingAnchor.constraint(equalTo: trailingAnchor),
+            rightInstance.bottomAnchor.constraint(equalTo: bottomAnchor),
+            rightInstance.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+    
+    func setPercentage(_ percentage: CGFloat) {
+        rightInstance.setOffsetPercentage(percentage)
+        leftInstance.setOffsetPercentage(percentage)
+    }
+}
+
+private final class RippleReplicatorInstance: UIView {
+    enum Direction {
+        case left
+        case right
+    }
+    
+    override class var layerClass: AnyClass {
+        CAReplicatorLayer.self
+    }
+    
+    // MARK: Private Properties
+    private var replicatorLayer: CAReplicatorLayer {
+        layer as! CAReplicatorLayer
+    }
+    
+    //MARK:- View Properties
+    private let rippleShape = CAShapeLayer()
+    
+    private let rippleTranslation: CGFloat = 50
+    
+    //MARK:- Size constants and Margins
+    private var arcRadius: CGFloat {
+        bounds.height/2
+    }
+
+    private var rippleOffset: CGFloat {
+        rippleSize.width - arcRadius
+    }
+
+    private let numberOfRipples: Int = 3
+    
+    // MARK: Public Properties
+    override var tintColor: UIColor! {
+        didSet {
+            updateRipple()
+        }
+    }
+    
+    var direction: Direction = .left {
+        didSet {
+            updateRipple()
+        }
+    }
+    
+    var rippleSize: CGSize {
+        let rippleWidth = bounds.width/CGFloat(numberOfRipples)
+        return CGSize(width: rippleWidth + arcRadius*0.35, height: bounds.height)
+    }
+    
+    /// Use this property to change the linewidth of child ripples
+    /// Default value is `1`
+    var rippleLineWidth: CGFloat = 1 {
+        didSet {
+            updateRipple()
+        }
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    // MARK: Public Functions
+    func setup() {
+        rippleShape.fillColor = nil
+        replicatorLayer.addSublayer(rippleShape)
+        replicatorLayer.instanceCount = numberOfRipples
+    }
+    
+    /// It will update the spacing between each ripples by taking
+    /// ``direction``, `rippleOffset`` and ``rippleOffsetPercentage`` applied to `x`
+    func setOffsetPercentage(_ percentage: CGFloat) {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        switch direction {
+        case .left:
+            replicatorLayer.instanceTransform = CATransform3DMakeTranslation(rippleOffset * percentage, 0, 0)
+        case .right:
+            replicatorLayer.instanceTransform = CATransform3DMakeTranslation(-1 * rippleOffset * (1-percentage), 0, 0)
+        }
+        CATransaction.commit()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let path: UIBezierPath
+        switch direction {
+        case .left:
+            path = UIBezierPath(arcCenter: CGPoint(x: arcRadius, y: rippleSize.height/2), radius: rippleSize.height/2 - rippleLineWidth/2, startAngle: 3 * .pi/2, endAngle: .pi/2, clockwise: false)
+
+            path.addLine(to: CGPoint(x: rippleSize.width, y: bounds.height-rippleLineWidth/2))
+            path.move(to: CGPoint(x: arcRadius, y: rippleLineWidth/2))
+            path.addLine(to: CGPoint(x: rippleSize.width, y: rippleLineWidth/2))
+
+        case .right:
+            let arcOriginX = rect.width - arcRadius
+
+            path = UIBezierPath(arcCenter: CGPoint(x: arcOriginX, y: rippleSize.height/2), radius: rippleSize.height/2 - rippleLineWidth/2, startAngle: 3 * .pi/2, endAngle: .pi/2, clockwise: direction == .right)
+
+            path.addLine(to: CGPoint(x: 0, y: bounds.height-rippleLineWidth/2))
+            path.move(to: CGPoint(x: arcOriginX, y: rippleLineWidth/2))
+            path.addLine(to: CGPoint(x: 0, y: rippleLineWidth/2))
+
+        }
+
+        rippleShape.path = path.cgPath
+    }
+
+    // MARK: Private Functions
+    
+    /// It will update the ripple related properties and frame
+    private func updateRipple() {
+        rippleShape.strokeColor = tintColor.cgColor
+        rippleShape.lineWidth = rippleLineWidth
+        let xOrigin = direction == .left ? 0 : bounds.width - rippleSize.width
+        rippleShape.frame = CGRect(x: xOrigin, y: 0, width: rippleSize.width, height: rippleSize.height)
+    }
+}
+
--- RippleScrollBarDemo/ViewController.swift
+++ RippleScrollBarDemo/ViewController.swift
@@ -7,13 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 250, height: 300)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: String(describing: UICollectionViewCell.self))
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 15)
+        return collectionView
+    }()
+    private let scrollBar = RippleScrollBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(red: 241/255, green: 237/255, blue: 228/255, alpha: 1.0)
+
+        collectionView.delegate = self
+        collectionView.backgroundColor = view.backgroundColor
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        view.addSubview(scrollBar)
+        scrollBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollBar.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor),
+            scrollBar.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 20),
+            scrollBar.widthAnchor.constraint(equalToConstant: 200),
+            scrollBar.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        view.layoutIfNeeded()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        scrollViewDidScroll(collectionView)
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: UICollectionViewCell.self), for: indexPath)
+        cell.backgroundColor = UIColor.white
+        cell.layer.cornerRadius = 5
+        cell.layer.borderColor = UIColor.gray.cgColor
+        cell.layer.borderWidth = 1
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let contentInset = scrollView.contentInset
+        let contentOffset = scrollView.contentOffset
+        let contentSize = scrollView.contentSize
+        let currentContentOffset = contentOffset.x + contentInset.left
+
+        let totalContentOffset = contentSize.width + contentInset.left + contentInset.right - view.frame.width
+        
+        scrollBar.setPercentage(max(min(currentContentOffset/totalContentOffset, 1.0), 0))
+    }
+}
